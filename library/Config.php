a6ffc4aee50cbe4cc88c8985cbe28e6b592acb6e
fan yang <koopa@qq.com>
2013-08-28 17:14:32 -0400

    remove password

--- library/Config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST','dealforgirl.db.10217542.hostedresource.com');
-define('DB_USER','dealforgirl');
-define('DB_PASSWORD','Sherrytao88!');
-define('DB_NAME','dealforgirl');
-define('DB_CHARSET','utf8');
-?>
--- library/Config1.php
+++ library/Config1.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST','localhost');
 define('DB_USER','dealforg');
-define('DB_PASSWORD','Sherrytao88!!');
+define('DB_PASSWORD','');
 define('DB_NAME','dealforg_dealforgirl');
 define('DB_CHARSET','utf8');
 ?>
